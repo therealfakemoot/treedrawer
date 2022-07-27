ac5b2d4dbc5cb94f50af4c9d9639cc621692bc24
Nick Dumas <dumasn@homes.com>
2022-07-27 14:46:58 -0400

    first draft at mutating a tree

--- examples/randomtree.go
+++ examples/randomtree.go
@@ -3,31 +3,45 @@ package main
 import (
 	"fmt"
 	"log"
-	"strings"
+	"math/rand"
+	// "strings"
 
 	"github.com/therealfakemoot/treedrawer/tree"
-	"golang.org/x/net/html"
 )
 
-func main() {
-	s := `<p>Links:</p><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul>`
-	doc, err := html.Parse(strings.NewReader(s))
-	if err != nil {
-		log.Fatal(err)
+type MutateOpts struct {
+	MinWidth, MaxWidth int
+	MinDepth, MaxDepth int
+}
+
+func Mutate(t *tree.Tree, depth int, mo MutateOpts) {
+	log.Printf("Mutating tree at depth %d\n", depth)
+	if depth > mo.MaxDepth {
+		log.Printf("trying to exit recursion: %t\n", depth > mo.MaxDepth)
+		return
 	}
-
-	t := tree.NewTree(tree.NodeString(""))
-
-	var f func(*html.Node, *tree.Tree)
-	f = func(n *html.Node, t *tree.Tree) {
-		t.SetVal(tree.NodeString(n.Data))
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			tChild := t.AddChild(tree.NodeString(""))
-			f(c, tChild)
+	for i := 0; i < mo.MaxWidth; i++ {
+		if rand.Float64() > .5 {
+			return
 		}
+		log.Printf("Creating node (%d,%d)\n", depth, i)
+		subtree := tree.NewTree(tree.NodeString(fmt.Sprintf("%d,%d", depth, i)))
+		Mutate(subtree, depth+1, mo)
+		t.AddChildNode(subtree)
 	}
-	// Starting from FirstChild because the DocumentRoot has an empty Val
-	f(doc.FirstChild, t)
 
+}
+
+func main() {
+
+	var ()
+	t := tree.NewTree(tree.NodeString("0"))
+	mo := MutateOpts{
+		MinDepth: 4,
+		MinWidth: 2,
+		MaxDepth: 9,
+		MaxWidth: 6,
+	}
+	Mutate(t, 0, mo)
 	fmt.Println(t)
 }
